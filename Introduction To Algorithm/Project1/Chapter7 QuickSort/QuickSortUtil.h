a1ee242c90ad932cc7eefadd8716a27a40a35dca
liweiyadeclot <364728529@qq.com>
2023-11-17 15:26:03 +0800

    Update QuickSort and ER handbook

--- Introduction To Algorithm/Project1/Chapter7 QuickSort/QuickSortUtil.h
+++ Introduction To Algorithm/Project1/Chapter7 QuickSort/QuickSortUtil.h
@@ -31,4 +31,73 @@ namespace Chapter7
 		}
 		
 	}
+
+	int HoarePartition(std::vector<int>& A, int p, int r)
+	{
+		int x = A[p];
+		int i = p - 1;
+		int j = r + 1;
+		while (true)
+		{
+			do
+				j--;
+			while (A[j] > x);
+
+			do
+				i++;
+			while (A[i] < x);
+
+			if (i < j)
+				std::swap(A[i], A[j]);
+			else
+				return j;
+		}
+
+	}
+
+	void HoareQuickSort(std::vector<int>&A, int p, int r)
+	{
+		if (p < r)
+		{
+			int q = HoarePartition(A, p, r);
+			HoareQuickSort(A, p, q);
+			HoareQuickSort(A, q + 1, r);
+		}
+		return;
+	}
+
+	void PartitionWithEqualElem(std::vector<int>& A, int p, int r)
+	{
+		int x = A[p];
+		int low = p;
+		int high = p;
+		for (int j = p + 1; j <= r; ++j)
+		{
+			if (A[j] == x)
+			{
+				std::swap(A[high + 1], A[j]);
+				high++;
+			}
+			else if (A[j] < x)
+			{
+				A[high]
+			}
+		}
+	}
+
+	void TailRecursiveQuickSort(std::vector<int>& A, int p, int r)
+	{
+		while (p < r)
+		{
+			// Partition and sort left subarray
+			int q = Partition(A, p, r);
+			TailRecursiveQuickSort(A, p, q - 1);
+			p = q + 1;
+		}
+	}
+
+	void TRQWithBetterStackDeepth(std::vector<int>& A, int p, int r)
+	{
+
+	}
 }
--- Introduction To Algorithm/Project1/Chapter7 QuickSort/test.cpp
+++ Introduction To Algorithm/Project1/Chapter7 QuickSort/test.cpp
@@ -9,8 +9,9 @@ using namespace std;
 int main(void)
 {
 	vector<int> A{ 9, 12, 3, 0, 5 };
+	//vector<int> A{ 5, 0, 3 };
 
-	Chapter7::QuickSort(A, 0, A.size() - 1);
+	Chapter7::HoareQuickSort(A, 0, A.size() - 1);
 	VectorUtils::PrintElems(A);
 	return 0;
 	
